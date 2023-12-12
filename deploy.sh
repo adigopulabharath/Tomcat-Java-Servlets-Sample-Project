6a9fe223c150e6ade8dc21ccdca0a7621f16baa9
adigopulabharath <153508760+adigopulabharath@users.noreply.github.com>
2023-12-12 14:51:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Variables
-TOMCAT_HOME=/path/to/tomcat   # Replace with the actual path to your Tomcat installation
-WEBAPP_NAME=sample-project    # Replace with the name of your web application
-WAR_FILE=target/sample-project.war  # Replace with the name of your generated WAR file
+TOMCAT_HOME=/opt/tomcat
+WEBAPP_NAME=sample-project
+WAR_FILE=target/sample-project.war
 
 # Stop Tomcat
 $TOMCAT_HOME/bin/shutdown.sh
