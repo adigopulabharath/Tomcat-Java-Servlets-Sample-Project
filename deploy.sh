a6a9b7eab3322f3818f9cdc1b5bc741534b00aee
adigopulabharath <153508760+adigopulabharath@users.noreply.github.com>
2023-12-12 16:34:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 # Variables
 TOMCAT_HOME=/opt/tomcat
 WEBAPP_NAME=tomcat-java-servlets-sample-project
-WAR_FILE=target/sample-project-1.0-SNAPSHOT.war
+WAR_FILE=target/tomcat-java-servlets-sample-project.war
+JAR_FILE=target/sample-project-1.0-SNAPSHOT.jar
 
 # Stop Tomcat
 $TOMCAT_HOME/bin/shutdown.sh
@@ -13,6 +14,9 @@ rm -rf $TOMCAT_HOME/webapps/$WEBAPP_NAME*
 
 # Copy the new WAR file
 cp $WAR_FILE $TOMCAT_HOME/webapps/
+
+# Make the JAR file executable
+chmod +x $JAR_FILE
 
 # Start Tomcat
 $TOMCAT_HOME/bin/startup.sh
